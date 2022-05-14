1f4c4493984f5e6cc0ecad887a268a7c8e59d5d1
SmiIemaker <1143609522@qq.com>
2022-05-14 19:06:00 +0800

    提交文件

---
+++ src/pages/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="home">
+        <Stage />
+        <LeftSide />
+        <RightSide />
+        <TextureView />
+        <MenuView />
+        <FileView />
+        <GltfConfig />
+        <DragView />
+    </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import Stage from "@/components/Stage/index.vue";
+import LeftSide from "@/components/LeftSide/index.vue";
+import RightSide from "@/components/RightSide/index.vue";
+import TextureView from "@/components/TextureView/index.vue";
+import MenuView from "@/components/MenuView/index.vue";
+import FileView from "@/components/FileView/index.vue";
+import GltfConfig from "@/components/GltfConfig/index.vue";
+import DragView from "@/components/DragView/index.vue";
+import GameEvent from "@/core/event/index";
+
+export default {
+    data() {
+        return {
+            
+        };
+    },
+    components: {
+        Stage,
+        LeftSide,
+        RightSide,
+        TextureView,
+        MenuView,
+        FileView,
+        GltfConfig,
+        DragView
+    },
+    mounted() {
+        
+    }
+};
+</script>
